89c715f877a537561ad66f2f4e99aab94e492d6d
刘焕 <liuhuan74@meituan.com>
2022-11-04 18:04:30 +0800

    second commit

--- src/main/java/windowAndTime/WindowAggregateFunctionExample.java
+++ src/main/java/windowAndTime/WindowAggregateFunctionExample.java
@@ -1,6 +1,17 @@
 package windowAndTime;
 
+import dataStreamApi.ClickSource;
+import dataStreamApi.Event;
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.SlidingAlignedProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+
+import java.util.HashSet;
 
 /**
  * @author liuhuan
@@ -11,8 +22,45 @@ public class WindowAggregateFunctionExample {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
 
-//        env.addSource(new ClickSource())
-//                .assignTimestampsAndWatermarks(WatermarkStrategy.forMonotonousTimestamps())
+        SingleOutputStreamOperator<Event> stream = env.addSource(new ClickSource())
+                .assignTimestampsAndWatermarks(WatermarkStrategy.<Event>forMonotonousTimestamps()
+                        .withTimestampAssigner(new SerializableTimestampAssigner<Event>() {
+                            @Override
+                            public long extractTimestamp(Event element, long recordTimestamp) {
+                                return element.timestamp;
+                            }
+                        }));
+        // 所有数据设置相同的 key，发送到同一个分区统计 PV 和 UV，再相除
+        stream.keyBy(data -> true)
+                .window(SlidingAlignedProcessingTimeWindows.of(Time.seconds(10),
+                        Time.seconds(2)))
+                .aggregate(new AvgPv())
+                .print();
+        env.execute();
+    }
 
+    private static class AvgPv implements AggregateFunction<Event, Tuple2<HashSet<String>,Long>, Double> {
+        @Override
+        public Tuple2<HashSet<String>, Long> createAccumulator() {
+            return Tuple2.of(new HashSet<>(),0L);
+        }
+
+        @Override
+        public Tuple2<HashSet<String>, Long> add(Event value, Tuple2<HashSet<String>, Long> accumulator) {
+            // 属于本窗口的数据来一条累加一次，并返回累加器
+            accumulator.f0.add(value.user);
+            return Tuple2.of(accumulator.f0, accumulator.f1+1L);
+        }
+
+        @Override
+        public Double getResult(Tuple2<HashSet<String>,Long> accumulator) {
+            // 窗口闭合时，增量聚合结束，将计算结果发送到下游
+            return (double) accumulator.f1/accumulator.f0.size();
+        }
+
+        @Override
+        public Tuple2<HashSet<String>,Long> merge(Tuple2<HashSet<String>,Long> acc, Tuple2<HashSet<String>,Long> acc1) {
+            return null;
+        }
     }
 }
